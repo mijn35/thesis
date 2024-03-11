57040c6bc6a9c6a7db0f531f3ee1d1dfd010a2d6
Joren Mangelschots <joren.mangelschots@student.kuleuven.be>
2024-03-11 17:41:56 +0100

    cleaznre code + readme file

--- dynamicTest.py
+++ dynamicTest.py
@@ -17,7 +17,7 @@ import mediapipe as mp
 from utils import CvFpsCalc
 from model import KeyPointClassifier
 
-
+current_alphabet_index = 0
 def get_args():
     parser = argparse.ArgumentParser()
 
@@ -41,11 +41,11 @@ def get_args():
 
 
 def main():
-    # Argument parsing #################################################################
-    args = get_args()
+    counter = 0
     sign = -1
     letter = 0
-
+    # Argument parsing #################################################################
+    args = get_args()
     cap_device = args.device
     cap_width = args.width
     cap_height = args.height
@@ -90,109 +90,76 @@ def main():
     #  ########################################################################
     mode = 0
 
-    # Open CSV file for logging
+    """"# Open CSV file for logging
     csv_path = 'hand_landmarks_test.csv'
     with open(csv_path, 'a', newline="") as csv_file:
-        csv_writer = csv.writer(csv_file)
+        csv_writer = csv.writer(csv_file)"""
 
-        start_time = time.time()
-        frame_count = 0
-        counter = 0
+    start_time = time.time()
+    frame_count = 0
 
-        while True:
-            fps = cvFpsCalc.get()
+    while True:
+        fps = cvFpsCalc.get()
 
-            # Process Key (ESC: end) #################################################
-            key = cv.waitKey(10)
-            if key == 27:  # ESC
-                break
-            number, mode = select_mode(key, mode)
-            if number != -1:
-                sign = number
+        # Process Key (ESC: end) #################################################
+        key = cv.waitKey(10)
+        if key == 27:  # ESC
+            break
+        number, mode = select_mode(key, mode)
+        if number != -1:
+            sign = number
 
-            # Camera capture #####################################################
-            ret, image = cap.read()
+        # Camera capture #####################################################
+        ret, image = cap.read()
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-            """
-            #HUE OFFSETS Take 80
-            hue_offset = 1
-            img_hsv = cv2.cvtColor(image, cv.COLOR_BGR2HSV)
-            h, s, v = cv2.split(img_hsv)
-            # shift the hue
-            h = (h + hue_offset) % 180
-            #    combine new hue with s and v
-            img_hsv = cv2.merge([h, s, v])
-            # convert from HSV to BGR
-            img_with_hue = cv2.cvtColor(img_hsv, cv.COLOR_HSV2BGR)
-            image_rgb = cv2.cvtColor(img_with_hue, cv2.COLOR_BGR2RGB)"""
+        if not ret:
+            break
 
-            image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_rgb = cv.flip(image_rgb, 1)
+        results = hands.process(image_rgb)
+        debug_image = cv.flip(image, 1)
+        #  ####################################################################
+        if time.time() - start_time >= 0.02:  # 20ms passed
+            frame_count += 1
+            start_time = time.time()  # Reset the timer
+            if results.multi_hand_landmarks is not None:
+                for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
+                                                      results.multi_handedness):
+                    # Bounding box calculation
+                    brect = calc_bounding_rect(debug_image, hand_landmarks)
+                    # Landmark calculation
+                    landmark_list = calc_landmark_list(debug_image, hand_landmarks)
 
-            if not ret:
-                break
-            """image_rgb = cv.flip(image_rgb, 1)  # Mirror display
-            debug_image = copy.deepcopy(image_rgb)
+                    # Conversion to relative coordinates / normalized coordinates
+                    pre_processed_landmark_list = pre_process_landmark(
+                        landmark_list)
+                    # Write to the dataset file
+                    counter, sign, letter = logging_csv(sign, mode, pre_processed_landmark_list, counter, letter)
+                    if letter == 25:
+                        cap.release()
+                        cv.destroyAllWindows()
 
-            image_rgb.flags.writeable = False
-            results = hands.process(image_rgb)
-            image_rgb.flags.writeable = True"""
-            image_rgb = cv.flip(image_rgb, 1)
-            results = hands.process(image_rgb)
-            debug_image = cv.flip(image, 1)
-            #  ####################################################################
-            if time.time() - start_time >= 0.02:  # 20ms passed
-                frame_count += 1
-                start_time = time.time()  # Reset the timer
-                if results.multi_hand_landmarks is not None:
-                    for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
-                                                          results.multi_handedness):
-                        # Bounding box calculation
-                        brect = calc_bounding_rect(debug_image, hand_landmarks)
-                        # Landmark calculation
-                        landmark_list = calc_landmark_list(debug_image, hand_landmarks)
+                    # Hand sign classification
+                    hand_sign_id, probability = keypoint_classifier(pre_processed_landmark_list)
 
-                        # Conversion to relative coordinates / normalized coordinates
-                        pre_processed_landmark_list = pre_process_landmark(
-                            landmark_list)
-                        pre_processed_point_history_list = pre_process_point_history(
-                            debug_image, point_history)
-                        # Write to the dataset file
-                        # logging_csv(number, mode, pre_processed_landmark_list, pre_processed_point_history_list)
-                        if mode == 1 and sign != -1:
-                            csv_writer.writerow([sign, *pre_processed_landmark_list])
-                            counter += 1
-                            if counter == 10:
-                                sign = -1
-                                letter += 1
-                                if letter == 9:
-                                    letter += 1
-                                if letter == 25:
-                                    cap.release()
-                                    cv.destroyAllWindows()
-                                counter = 0
+                    # Drawing part
+                    debug_image = draw_bounding_rect(use_brect, debug_image, brect)
+                    debug_image = draw_landmarks(debug_image, landmark_list)
+                    debug_image = draw_info_text(
+                        debug_image,
+                        brect,
+                        handedness,
+                        keypoint_classifier_labels[hand_sign_id],
+                        probability,
+                    )
+            else:
+                point_history.append([0, 0])
 
-                        # Hand sign classification
-                        hand_sign_id, probability = keypoint_classifier(pre_processed_landmark_list)
-                        print(probability)
+        debug_image = draw_info(debug_image, fps, mode, number, counter, keypoint_classifier_labels[letter])
 
-                        # Drawing part
-                        debug_image = draw_bounding_rect(use_brect, debug_image, brect)
-                        debug_image = draw_landmarks(debug_image, landmark_list)
-                        debug_image = draw_info_text(
-                            debug_image,
-                            brect,
-                            handedness,
-                            keypoint_classifier_labels[hand_sign_id],
-                            probability,
-                        )
-                else:
-                    point_history.append([0, 0])
-
-            debug_image = draw_point_history(debug_image, point_history)
-            debug_image = draw_info(debug_image, fps, mode, number, counter, keypoint_classifier_labels[letter])
-
-            # Screen reflection #############################################################
-            cv.imshow('Hand Gesture Recognition', debug_image)
+        # Screen reflection #############################################################
+        cv.imshow('Hand Gesture Recognition', debug_image)
 
     cap.release()
     cv.destroyAllWindows()
@@ -212,8 +179,6 @@ def select_mode(key, mode):
             mode = 0
         else:
             mode = 1
-    if key == 9:  # tab
-        mode = 2
     return number, mode
 
 
@@ -234,7 +199,6 @@ def calc_bounding_rect(image, landmarks):
 
     return [x, y, x + w, y + h]
 
-
 def calc_landmark_list(image, landmarks):
     image_width, image_height = image.shape[1], image.shape[0]
 
@@ -249,7 +213,6 @@ def calc_landmark_list(image, landmarks):
         landmark_point.append([landmark_x, landmark_y])
 
     return landmark_point
-
 
 def pre_process_landmark(landmark_list):
     temp_landmark_list = copy.deepcopy(landmark_list)
@@ -277,46 +240,21 @@ def pre_process_landmark(landmark_list):
 
     return temp_landmark_list
 
-
-def pre_process_point_history(image, point_history):
-    image_width, image_height = image.shape[1], image.shape[0]
-
-    temp_point_history = copy.deepcopy(point_history)
-
-    # Convert to relative coordinates
-    base_x, base_y = 0, 0
-    for index, point in enumerate(temp_point_history):
-        if index == 0:
-            base_x, base_y = point[0], point[1]
-
-        temp_point_history[index][0] = (temp_point_history[index][0] -
-                                        base_x) / image_width
-        temp_point_history[index][1] = (temp_point_history[index][1] -
-                                        base_y) / image_height
-
-    # Convert to a one-dimensional list
-    temp_point_history = list(
-        itertools.chain.from_iterable(temp_point_history))
-
-    return temp_point_history
-
-
-def logging_csv(number, mode, landmark_list, point_history_list):
-    if mode == 0:
-        pass
-    if mode == 1 and (0 <= number <= 34):
-        csv_path = 'model/keypoint_classifier/test.csv'
-        # csv_path = 'model/keypoint_classifier/keypoint.csv'
-        with open(csv_path, 'a', newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow([number, *landmark_list])
-    if mode == 2 and (0 <= number <= 34):
-        csv_path = 'model/point_history_classifier/point_history.csv'
-        with open(csv_path, 'a', newline="") as f:
-            writer = csv.writer(f)
-            writer.writerow([number, *point_history_list])
-    return
-
+def logging_csv(sign, mode, landmark_list, counter, letter):
+    # Open CSV file for logging
+    csv_path = 'hand_landmarks_test1.csv'
+    if mode == 1 and sign != -1:
+        with open(csv_path, 'a', newline="") as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow([sign, *landmark_list])
+            counter += 1
+            if counter == 10:
+                sign = -1
+                letter += 1
+                if letter == 9:
+                    letter += 1
+                counter = 0
+    return counter, sign, letter
 
 def draw_landmarks(image, landmark_point):
     if len(landmark_point) > 0:
@@ -529,18 +467,6 @@ def draw_info_text(image, brect, handedness, hand_sign_text, probability):
                cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
 
     return image
-
-
-def draw_point_history(image, point_history):
-    for index, point in enumerate(point_history):
-        if point[0] != 0 and point[1] != 0:
-            cv.circle(image, (point[0], point[1]), 1 + int(index / 2),
-                      (152, 251, 152), 2)
-
-    return image
-
-
-current_alphabet_index = 0
 
 def draw_info(image, fps, mode, number, counter, sign):
     global current_alphabet_index
