eba596d51cfc526aa0180041363bee4ef545f6d9
Joren Mangelschots <joren.mangelschots@student.kuleuven.be>
2024-04-04 09:53:36 +0200

    added datamoeke&vake

--- dynamicTest.py
+++ dynamicTest.py
@@ -237,7 +237,7 @@ def pre_process_landmark(landmark_list):
 
 def logging_csv(sign, mode, landmark_list, counter, letter):
     # Open CSV file for logging
-    csv_path = 'hand_landmarks_bla' \
+    csv_path = 'hand_landmarks_moeke' \
                '.csv'
     if mode == 1 and sign != -1:
         with open(csv_path, 'a', newline="") as csv_file:
