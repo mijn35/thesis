95c8fcc49bf450bf08a7fd054266422565962ae3
Joren Mangelschots <joren.mangelschots@student.kuleuven.be>
2024-05-01 09:54:31 +0200

    data changed to correct format

--- dynamicTest.py
+++ dynamicTest.py
@@ -237,7 +237,7 @@ def pre_process_landmark(landmark_list):
 
 def logging_csv(sign, mode, landmark_list, counter, letter):
     # Open CSV file for logging
-    csv_path = 'camera_data/hand_landmarks_chadwick.csv'
+    csv_path = 'hand_landmarks_h.csv'
     if mode == 1 and sign != -1:
         with open(csv_path, 'a', newline="") as csv_file:
             csv_writer = csv.writer(csv_file)
